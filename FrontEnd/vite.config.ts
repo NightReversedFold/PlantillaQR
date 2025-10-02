92edd155df716b301e2d6a5eaec563f0b1ebdc75
NightReversedFold <angel74977@gmail.com>
2025-10-01 21:31:24 -0600

    final

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -8,7 +8,7 @@ export default ({ mode }: { mode: string }) => {
   return defineConfig({
 
     plugins: [react(), tailwindcss()],
-    base: '/PlantillaQR/',
+    base: '/PlantillaQR',
     server: {
       allowedHosts: true
     },
