40aebabc0c22d0205a2c84b42139079f373e7419
NightReversedFold <angel74977@gmail.com>
2025-10-01 18:38:54 -0600

    r31

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 
 import dotenv from 'dotenv';
-dotenv.config();
 
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config(); 
+}
 const PORT = process.env.PORT;
 
 import express from 'express';
